6a1bbc37685c210513cab68d87439523cc3d6b77
Mark Lifshits <limar.go@gmail.com>
2019-03-08 14:19:58 +0800

    Initial

---
+++ parseblock.py
@@ -0,0 +1,218 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  7 12:25:05 2019
+
+@author: marklifshits
+"""
+
+'''
+    - should we check Meassage header checksum?
+'''
+
+import struct
+import binascii
+from collections import namedtuple
+from io import SEEK_SET, SEEK_CUR
+from ctypes import Structure, c_uint32, c_int32, c_int64, c_uint16, c_char, c_byte, sizeof
+from io import BytesIO
+
+
+class ReadableUnit:
+    @classmethod
+    def from_stream(cls, stream):
+        raise AssertionError('Not implemented.')
+
+class ReadableBufferObject(ReadableUnit):
+    @classmethod
+    def from_stream(cls, stream):
+        inst = cls()
+        stream.readinto(inst)
+        return inst
+
+class MessageHeader(Structure, ReadableBufferObject):
+    _fields_ = [ ('magic', c_uint32),
+                 ('command', c_char*12),
+                 ('length', c_uint32),
+                 ('checksum', c_uint32)
+               ]
+    
+
+class OutPoint(Structure, ReadableBufferObject):
+    _fields_ = [ ('hash', c_byte*32),
+                 ('index', c_uint32)
+               ]
+    
+class TxIn(ReadableUnit):
+    @classmethod
+    def from_stream(cls, stream):
+        inst = TxIn()
+        inst.previous_output = OutPoint.from_stream(stream)
+        inst.script_length = read_varint(stream)
+        # skip script
+        stream.seek(inst.script_length, SEEK_CUR)
+        inst.sequence = c_uint32()
+        stream.readinto(inst.sequence)
+        return inst
+
+class TxOut(ReadableUnit):
+    @classmethod
+    def from_stream(cls, stream):
+        inst = TxOut()
+        inst.value = c_int64()
+        stream.readinto(inst.value)
+        inst.pk_script_length = read_varint(stream)
+        # skip pk_skript
+        stream.seek(inst.pk_script_length, SEEK_CUR)
+        return inst
+
+class TxWitness(ReadableUnit):
+
+    def __init__(self, data_components):
+        self.data_components = data_components
+
+    @classmethod
+    def from_stream(cls, stream):
+        
+        count_data_components = read_varint(stream)
+        data_components = []
+        for _ in range(count_data_components):
+            data_size = read_varint(stream)
+            data_components.append(data_size)
+            # skip data
+            stream.seek(data_size, SEEK_CUR)
+        return TxWitness(data_components)
+    
+class Transaction(ReadableUnit):
+#    TransactionTuple = namedtuple('TransactionTuple', 
+#                                  ('version', 
+#                                   'flag',
+#                                   'tx_in_count',
+#                                   'tx_in',
+#                                   'tx_out_count',
+#                                   'tx_out',
+#                                   'tx_witnesses',
+#                                   'lock_time'))
+    
+    TransactionTuple = namedtuple('TransactionTuple', 
+                                  ('version', 
+                                   'flag',
+                                   'tx_in_count',
+                                   'tx_out_count',
+                                   'lock_time',
+                                   'start_offset',
+                                   'end_offset'))
+        
+        
+    @classmethod
+    def from_stream(cls, stream):
+
+        start_offset = stream.tell()
+        version = c_int32()
+        stream.readinto(version)
+
+        flag = c_uint16()
+        stream.readinto(flag)
+        
+        if flag.value != 1:
+            stream.seek(- sizeof(flag), SEEK_CUR)
+            flag = None
+        else:
+            flag = flag.value
+        
+        tx_in_count = read_varint(stream)
+        # skip tx inputs
+        for i in range(tx_in_count):
+            print('\t tx_in', i, 'out of', tx_in_count)
+            _ = TxIn.from_stream(stream)
+        
+        tx_out_count = read_varint(stream)
+        # skip tx outputs
+        for _ in range(tx_out_count):
+            _ = TxOut.from_stream(stream)
+        
+        if flag is not None:
+            for _ in range(tx_in_count):
+                # skip witness
+                TxWitness.from_stream(stream)
+        
+        lock_time = c_uint32()
+        stream.readinto(lock_time)
+        
+        end_offset = stream.tell()
+        
+        return cls.TransactionTuple(version, flag, tx_in_count, tx_out_count, 
+                                lock_time, start_offset, end_offset)
+        
+            
+
+class BlockHeader(Structure):
+    _fields_ = [ ('version', c_int32),
+                 ('prev_block', c_byte*32),
+                 ('merkle_root', c_byte*32),
+                 ('timestamp', c_uint32),
+                 ('bits', c_uint32),
+                 ('nonce', c_uint32)
+            ]
+    
+    @classmethod
+    def from_stream(cls, stream):
+        inst = cls()
+        stream.readinto(inst)
+        inst.txn_count = read_varint(stream)
+        return inst
+    
+class HexStream():
+    def __init__(self, stream):
+        self.stream = stream
+    
+    def read(self, n):
+        text = self.stream.read(n*2)
+        return binascii.a2b_hex(text)
+    
+    def readinto(self, b):
+        size = sizeof(b)
+        data = self.read(size)
+        BytesIO(data).readinto(b)
+  
+    def tell(self):
+        return int(self.stream.tell()/2)
+    
+    def seek(self, offset, whence = SEEK_SET):
+        self.stream.seek(int(offset/2), whence)
+
+
+def read_varint(stream):
+    data = stream.read(1)
+    i = struct.unpack('B',data)[0]
+    if i < 0xfd:
+        return i
+    
+    bytes_to_read, fmt = {
+     0xfd: (2, '<H'),
+     0xfe: (4, '<I'),
+     0xff: (8, '<Q')
+    }[i]
+    
+    data = stream.read(bytes_to_read)
+    i = struct.unpack(fmt, data)[0]
+    return i
+
+       
+def read_message(stream):
+    message_header = MessageHeader.from_stream(stream)
+    if message_header.command.decode('ascii') == 'block':
+        block_header = BlockHeader.from_stream(stream)
+    for i in range(block_header.txn_count):
+        t = Transaction.from_stream(stream)
+        print('Transaction', i, 'from', block_header.txn_count, 'start', t.start_offset, 'end', t.end_offset)
+
+    return message_header
+
+def main():
+    with open('/Users/marklifshits/work/bitcoin-block/sample_block.txt', 'rb') as f:
+        hs = HexStream(f)
+        read_message(hs)
+
+if __name__ == '__main__':
+    main()
---
+++ parseblock_test.py
@@ -0,0 +1,36 @@
+import unittest
+from parseblock import BlockHeader, HexStream
+from io import BytesIO
+import binascii
+
+class GenesisBlock:
+    block = b'0100000000000000000000000000000000000000000000000000000000000000000000003BA3EDFD7A7B12B27AC72C3E67768F617FC81BC3888A51323A9FB8AA4B1E5E4A29AB5F49FFFF001D1DAC2B7C0101000000010000000000000000000000000000000000000000000000000000000000000000FFFFFFFF4D04FFFF001D0104455468652054696D65732030332F4A616E2F32303039204368616E63656C6C6F72206F6E206272696E6B206F66207365636F6E64206261696C6F757420666F722062616E6B73FFFFFFFF0100F2052A01000000434104678AFDB0FE5548271967F1A67130B7105CD6A828E03909A67962E0EA1F61DEB649F6BC3F4CEF38C4F35504E51EC112DE5C384DF7BA0B8D578A4C702B6BF11D5FAC00000000'
+    block_header = '0100000000000000000000000000000000000000000000000000000000000000000000003ba3edfd7a7b12b27ac72c3e67768f617fc81bc3888a51323a9fb8aa4b1e5e4a29ab5f49ffff001d1dac2b7c'
+    transaction_count = 1
+    block_hash = b'000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f'
+    prev_block = b'0000000000000000000000000000000000000000000000000000000000000000'
+    merkle_root = b'4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b'
+    version = 1
+    bits = 486604799
+    nonce = 2083236893
+
+class GenesisTransaction:
+    size = 204
+    bits = 486604799
+    nonce = 2083236893
+
+class Test_ParseBlockTests(unittest.TestCase):
+    def test_parse_genesis_block(self):
+        binary_stream = BytesIO(GenesisBlock.block)
+        stream = HexStream(binary_stream)
+        header = BlockHeader.from_stream(stream)
+        self.assertEqual(header.version, GenesisBlock.version)
+        self.assertEqual(header.bits, GenesisBlock.bits)
+        self.assertEqual(header.nonce, GenesisBlock.nonce)
+        self.assertEqual(binascii.b2a_hex(header.prev_block), GenesisBlock.prev_block)
+    def test2(self):
+        pass
+    pass# def test_genesis_block(self):
+        
+if __name__ == '__main__':
+    unittest.main()
